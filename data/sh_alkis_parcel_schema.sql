bfa57968b9c4dfefd665a2bc189bb84ba4983db7
Aurelius Wendelken <webtobesocial@gmail.com>
2024-10-29 10:20:08 +0100

    added further meta data from alkis parcels

--- data/sh_alkis_parcel_schema.sql
+++ data/sh_alkis_parcel_schema.sql
@@ -4,6 +4,16 @@ DROP TABLE IF EXISTS sh_alkis_parcel CASCADE;
 CREATE TABLE IF NOT EXISTS sh_alkis_parcel (
   id SERIAL,
   adv_id VARCHAR,
+  beginnt TIMESTAMP WITH TIME ZONE,
+  land VARCHAR,
+  regierungsbezirk INT,
+  kreis INT,
+  gemeinde VARCHAR,
+  gemarkungsnummer INT,
+  flurnummer INT,
+  nenner INT,
+  zaehler INT,
+  abweichender_rechtszustand BOOLEAN,
   wkb_geometry GEOMETRY(POLYGON, 4326),
   PRIMARY KEY(id)
 );
--- tools/insert_alkis_csv.py
+++ tools/insert_alkis_csv.py
@@ -7,6 +7,7 @@ import logging as log
 import psycopg2
 import csv
 
+from datetime import datetime
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -45,8 +46,26 @@ def connect_database(env_path):
         sys.exit(1)
 
 
+def str_to_bool(v):
+    return v.lower() in ('yes', 'true', 't', '1')
+
+
+def parse_datetime(s):
+    return datetime.strptime(s, "%Y-%m-%dT%H:%M:%SZ")
+
+
 def insert_row(cur, row):
     adv_id = row['adv_id']
+    beginnt = parse_datetime(row['beginnt']) if row['beginnt'] else None
+    land = row['land'] if row['land'] else None
+    regierungsbezirk = int(row['regierungsbezirk']) if row['regierungsbezirk'] else None
+    kreis = int(row['kreis']) if row['kreis'] else None
+    gemeinde = row['gemeinde'] if row['gemeinde'] else None
+    gemarkungsnummer = int(row['gemarkungsnummer']) if row['gemarkungsnummer'] else None
+    flurnummer = int(row['flurnummer']) if row['flurnummer'] else None
+    nenner = int(row['nenner']) if row['nenner'] else None
+    zaehler = int(row['zaehler']) if row['zaehler'] else None
+    abweichender_rechtszustand = str_to_bool(row['abweichender_rechtszustand']) if row['abweichender_rechtszustand'] else None
     wkt_geometry = row['wkt_geometry']
 
     # Parse the identifier and the polygon coordinates
@@ -56,12 +75,14 @@ def insert_row(cur, row):
     wkt_geometry = f'POLYGON(({coordinates}))'
 
     sql = '''
-        INSERT INTO sh_alkis_parcel (adv_id, wkb_geometry)
-        VALUES (%s, ST_AsBinary(ST_Transform(ST_GeomFromText(%s, 25832), 4326))) RETURNING id
+        INSERT INTO sh_alkis_parcel (adv_id, beginnt, land, regierungsbezirk, kreis, gemeinde,
+            gemarkungsnummer, flurnummer, nenner, zaehler, abweichender_rechtszustand, wkb_geometry)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, ST_AsBinary(ST_Transform(ST_GeomFromText(%s, 25832), 4326))) RETURNING id
     '''
 
     try:
-        cur.execute(sql, (adv_id, wkt_geometry))
+        cur.execute(sql, (adv_id, beginnt, land, regierungsbezirk, kreis, gemeinde,
+            gemarkungsnummer, flurnummer, nenner, zaehler, abweichender_rechtszustand, wkt_geometry))
 
         last_inserted_id = cur.fetchone()[0]
 
